9e6d10e97cc7e9bb8d0565da088fc0c4d9a8ada0
Pavlo Rospopa <prospopa@cogniance.com>
2019-06-05 19:01:27 +0300

    remove pagination support of products list

--- src/main/java/rospopa/strpz2/read/persistence/ReadProductRepository.java
+++ src/main/java/rospopa/strpz2/read/persistence/ReadProductRepository.java
@@ -1,9 +1,9 @@
 package rospopa.strpz2.read.persistence;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import rospopa.strpz2.read.domain.Product;
 
 @Repository
-public interface ReadProductRepository extends PagingAndSortingRepository<Product, Long> {
+public interface ReadProductRepository extends CrudRepository<Product, Long> {
 }
--- src/main/java/rospopa/strpz2/read/web/controller/ProductController.java
+++ src/main/java/rospopa/strpz2/read/web/controller/ProductController.java
@@ -3,15 +3,13 @@ package rospopa.strpz2.read.web.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFacade;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import rospopa.strpz2.read.persistence.ReadProductRepository;
 import rospopa.strpz2.read.web.dto.ProductDto;
+
+import java.util.List;
 
 import static rospopa.strpz2.read.web.controller.ProductController.ENDPOINT;
 
@@ -26,9 +24,8 @@ class ProductController {
     private final MapperFacade mapperFacade;
 
     @GetMapping
-    Page<ProductDto> getProducts(@PageableDefault(sort = "availableQuantity", direction = Sort.Direction.DESC)
-                                         Pageable pageable) {
-        log.debug("Received request to get page of products. Pageable: {}", pageable);
-        return readProductRepository.findAll(pageable).map(product -> mapperFacade.map(product, ProductDto.class));
+    List<ProductDto> getProducts() {
+        log.info("Received request to get list of products");
+        return mapperFacade.mapAsList(readProductRepository.findAll(), ProductDto.class);
     }
 }
